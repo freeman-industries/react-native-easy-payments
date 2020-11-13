317a1da45ce4ecaa32dddf096fd05629a4cab421
Alfie <runticle@gmail.com>
2020-11-13 10:31:23 +0000

    Add confirmCardSetup function and export as part of Intents object

---
+++ js/Intents/index.js
@@ -0,0 +1,27 @@
+import { NativeModules } from 'react-native';
+
+const { ReactNativePayments } = NativeModules;
+
+const IS_ANDROID = Platform.OS === 'android';
+
+const Intents = {
+	confirmCardSetup(methodData, cardParams) {
+		return new Promise((resolve, reject) => {
+		  if (IS_ANDROID) {
+			return resolve();
+		  }
+	
+		  ReactNativePayments.confirmCardSetup(
+			methodData,
+			cardParams,
+			(err, data) => {
+			  if (err) return reject(err);
+	
+			  resolve(data);
+			}
+		  );
+		});
+	  }
+}
+
+export { Intents };
--- js/index.js
+++ js/index.js
@@ -1,2 +1,3 @@
 export { default as PaymentRequest } from './PaymentRequest';
 export { PKPaymentButton } from './PKPaymentButton';
+export { Intents } from './Intents';
