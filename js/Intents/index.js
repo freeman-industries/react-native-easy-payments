0ac9578c62ed5b2c0997188f7eab14c934179769
Nabil Freeman <nabil@instafork.com>
2020-11-17 16:17:11 +0000

    protect against non string values in card params

--- js/Intents/index.js
+++ js/Intents/index.js
@@ -4,16 +4,25 @@ const { ReactNativePayments } = NativeModules;
 
 const IS_ANDROID = Platform.OS === 'android';
 
-export function savePaymentMethod(methodData, cardParams) {
-  return new Promise((resolve, reject) => {
-    if (IS_ANDROID) {
-      // TODO Android
-      return resolve();
-    }
+// we use this quick method to ensure that all the values inside the object are strings, which can cause some crashes with the Stripe SDK and strict types in Objective-C.
+const stringifyObject = obj => {
+  for (const key in obj) {
+    obj[key] += '';
+  }
 
+  return obj;
+};
+
+export async function savePaymentMethod(
+  methodData,
+  cardParams,
+) {
+  if (IS_ANDROID) return;
+
+  return new Promise((resolve, reject) => {
     ReactNativePayments.savePaymentMethod(
       methodData,
-      cardParams,
+      stringifyObject(cardParams),
       (err, data) => {
         if (err) return reject(err);
 
@@ -23,21 +32,18 @@ export function savePaymentMethod(methodData, cardParams) {
   });
 }
 
-export function confirmPayment(methodData, paymentParams) {
-	return new Promise((resolve, reject) => {
-		if (IS_ANDROID) {
-		  // TODO Android
-		  return resolve();
-		}
-	
-		ReactNativePayments.confirmPayment(
-		  methodData,
-		  paymentParams,
-		  (err, data) => {
-			if (err) return reject(err);
-	
-			resolve(data);
-		  },
-		);
-	  });
+export async function confirmPayment(methodData, paymentParams) {
+  if (IS_ANDROID) return;
+  
+  return new Promise((resolve, reject) => {
+    ReactNativePayments.confirmPayment(
+      methodData,
+      stringifyObject(paymentParams),
+      (err, data) => {
+        if (err) return reject(err);
+  
+        resolve(data);
+      },
+    );
+  });
 }
