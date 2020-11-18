fdf72b57bd590f358fb4c9039ada0d4ea79433c8
Nabil Freeman <nabil@instafork.com>
2020-11-18 12:53:55 +0000

    yegor's canMakePaymentsUsingNetworks check

--- js/PaymentRequest/index.js
+++ js/PaymentRequest/index.js
@@ -500,5 +500,7 @@ export default class PaymentRequest {
       getPlatformMethodData(JSON.parse(this._serializedMethodData), Platform.OS)
     );
   }
+
+  static canMakePaymentsUsingNetworks = NativePayments.canMakePaymentsUsingNetworks;
 }
 
